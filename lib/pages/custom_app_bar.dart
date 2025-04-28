e17bd94921fc13857adfa788696ec557c4246eeb
igorek <igor-titov-2004@mail.ru>
2025-04-28 18:00:21 +0300

    secc

--- lib/pages/custom_app_bar.dart
+++ lib/pages/custom_app_bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Function(int) onMenuTap;
@@ -7,101 +7,89 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key, required this.onMenuTap});
 
   @override
-  Size get preferredSize => const Size.fromHeight(100); // Увеличиваем высоту
+  Size get preferredSize => const Size.fromHeight(70); // Высота AppBar
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      elevation: 4,
-      backgroundColor: Colors.white,
+      elevation: 0,
+      backgroundColor: const Color.fromARGB(255, 0, 140, 255),
       foregroundColor: Colors.black,
+      toolbarHeight: 100,
       title: Row(
+        
         children: [
-          // Добавление мест для 3 картинок
-          Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(5),
-                child: Image.network(
-                  'https://i.ytimg.com/vi/-xE6bGyuudI/maxresdefault.jpg',
-                  width: 60,
-                  height: 60,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(width: 8),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(5),
-                child: Image.network(
-                  'https://i.ytimg.com/vi/-xE6bGyuudI/maxresdefault.jpg',
-                  width: 60,
-                  height: 60,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(width: 8),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(5),
-                child: Image.network(
-                  'https://i.ytimg.com/vi/-xE6bGyuudI/maxresdefault.jpg',
-                  width: 60,
-                  height: 60,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ],
-          ),
           const SizedBox(width: 10),
-          const Icon(Icons.sports_soccer, color: Colors.green),
+          const Icon(Icons.sports_soccer, color: Colors.deepPurple),
           const SizedBox(width: 10),
           const Text(
             'M.O.drid shop',
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
           ),
           const Spacer(),
+          const SizedBox(height: 10),
           // Кнопки с иконками над ними, расположенные горизонтально
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              _buildIconButton(Icons.home, 'Главная', 0),
-              _buildSvgButton('lib/images/soccer-player-striped-t-shirt-svgrepo-com.svg',  'Одежда', 1),
+              _buildButton(Icons.home, 'Главная', 0),
+              const SizedBox(width: 30),
+              _buildSvgButton('lib/images/soccer-player-striped-t-shirt-svgrepo-com.svg', 'Одежда', 1),
+              const SizedBox(width: 30),
               _buildSvgButton('lib/images/football-shoe-svgrepo-com.svg', 'Обувь', 2),
-              _buildIconButton(Icons.shopping_cart, 'Корзина', 3),
-              _buildIconButton(Icons.person, 'Профиль', 4),
+              const SizedBox(width: 30),
+              _buildButton(Icons.shopping_cart, 'Корзина', 3),
+              const SizedBox(width: 30),
+              _buildButton(Icons.person, 'Профиль', 4),
             ],
           ),
         ],
       ),
     );
   }
+
   Widget _buildSvgButton(String assetPath, String label, int index) {
-    return Column(
-      children: [
-         SizedBox(width: 70,),
-        IconButton(
-          icon: SvgPicture.asset(
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        padding: EdgeInsets.zero,
+      ),
+      onPressed: () => onMenuTap(index),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,  // Центрируем по вертикали
+        children: [
+          SvgPicture.asset(
             assetPath,
-            width: 24, // Установите нужный размер
+            width: 24,
             height: 24,
+            color: Colors.white,
           ),
-          onPressed: () => onMenuTap(index),
-        ),
-        Text(label, style: const TextStyle(color: Colors.black,fontSize: 12)),
-      ],
+          const SizedBox(height: 4), // Отступ между иконкой и текстом
+          Text(label, style: const TextStyle(color: Colors.white, fontSize: 14)),
+        ],
+      ),
     );
   }
 
-  Widget _buildIconButton(IconData icon, String label, int index) {
-    return Column(
-      
-      children: [
-        SizedBox(width: 70,),
-        IconButton(
-          icon: Icon(icon, color: Colors.black),
-          onPressed: () => onMenuTap(index),
-        ),
-        Text(label, style: const TextStyle(color: Colors.black,fontSize: 12)),
-      ],
+  Widget _buildButton(IconData icon, String label, int index) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        padding: EdgeInsets.zero,
+      ),
+      onPressed: () => onMenuTap(index),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,  // Центрируем по вертикали
+        children: [
+          Icon(icon, color: Colors.white, size: 25,),
+          const SizedBox(height: 4), // Отступ между иконкой и текстом
+          Text(label, style: const TextStyle(color: Colors.white, fontSize: 14)),
+        ],
+      ),
     );
   }
 }
